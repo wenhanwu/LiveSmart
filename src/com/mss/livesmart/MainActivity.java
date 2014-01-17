8162967c442133fe6072a002688a7f053c5fb8a0
Witty <wittchot@mss.icics.ubc.ca>
2014-01-16 18:29:36 -0800

    Added search and setting items to action bar

--- src/com/mss/livesmart/MainActivity.java
+++ src/com/mss/livesmart/MainActivity.java
@@ -3,6 +3,9 @@ package com.mss.livesmart;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -23,5 +26,28 @@ public class MainActivity extends Activity {
 						.show();
 			}
 		});
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            //action when search button is clicked
+	            return true;
+	        case R.id.action_settings:
+	            //action when setting button is clicked
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 }
