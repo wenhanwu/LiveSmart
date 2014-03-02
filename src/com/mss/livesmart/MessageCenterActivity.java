71bd4e1f41b3bf1c1dc316da882e0a6be9c22b74
Jenny Wang <mujenwang971@gmail.com>
2014-03-02 09:24:02 -0800

    Minor modifications

--- src/com/mss/livesmart/MessageCenterActivity.java
+++ src/com/mss/livesmart/MessageCenterActivity.java
@@ -11,10 +11,16 @@ import android.widget.ListView;
 
 
 public class MessageCenterActivity extends Activity {
+	
+	Bundle bundle;
+	String newMessage;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_center);
+        
+        displayNewMessage();
         
         ArrayList<String> myStringArray = new ArrayList<String>();
         myStringArray.add("First Suggestion");
@@ -29,40 +35,40 @@ public class MessageCenterActivity extends Activity {
         myStringArray.add("10th Suggestion");
         myStringArray.add("11th Suggestion");
         myStringArray.add("12th Suggestion");
-        
-		Bundle bundle = getIntent().getExtras();
-		String newMessage = "no message";
-		if(bundle != null){
+       
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        listView.setAdapter(adapter);  
+	}
+	
+	private void displayNewMessage() {
+
+		// get the message
+		bundle = getIntent().getExtras();
+		newMessage = "no message";
+		if (bundle != null) {
 			newMessage = bundle.getString("key");
 		}
-        
+
 		// add a dialog and set message
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-		alertDialogBuilder.setTitle("New Recommendtion!");
-		alertDialogBuilder
-			.setMessage(newMessage)
-			.setCancelable(false)
-			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-				
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					// if "OK" is clicked, close current activity
-					MessageCenterActivity.this.finish();				
-				}
-			}) ;
-	
+		alertDialogBuilder.setTitle("New Recommendation!");
+		alertDialogBuilder.setMessage(newMessage).setCancelable(false)
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// if "OK" is clicked, close the dialog window
+						dialog.cancel();
+					}
+				});
+
 		// create and show the alert dialog
 		AlertDialog alertDialog = alertDialogBuilder.create();
 		alertDialog.show();
 		
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray);
-        ListView listView = (ListView) findViewById(R.id.listView1);
-        listView.setAdapter(adapter);
-        
-        // TODO: displays a new message window
-        
-        // TODO: after the message window is dismissed, add the new message to list
+
+		// TODO: after the message window is dismissed, add the new message to list
 	}
-	
-	
+
 }
