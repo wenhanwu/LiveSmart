f8455a243de3d3f49192e25a383d1115b2d5c8f3
Jenny Wang <mujenwang971@gmail.com>
2014-03-04 15:17:45 -0800

    Fixed "no new message" dialog, added new message to the list

--- src/com/mss/livesmart/MessageCenterActivity.java
+++ src/com/mss/livesmart/MessageCenterActivity.java
@@ -1,6 +1,7 @@
 package com.mss.livesmart;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -14,41 +15,45 @@ public class MessageCenterActivity extends Activity {
 	
 	Bundle bundle;
 	String newMessage;
+	ArrayList<String> messageArray;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_center);
         
-        displayNewMessage();
+        ArrayList<String> messageArray = new ArrayList<String>();
+        messageArray.add("First Suggestion");
+        messageArray.add("2nd Suggestion");
+        messageArray.add("3rd Suggestion");
+        messageArray.add("4th Suggestion");
+        messageArray.add("5th Suggestion");
+        messageArray.add("6th Suggestion");
+        messageArray.add("7th Suggestion");
+        messageArray.add("8th Suggestion");
+        messageArray.add("9th Suggestion");
+        messageArray.add("10th Suggestion");
+        messageArray.add("11th Suggestion");
+        messageArray.add("12th Suggestion");
         
-        ArrayList<String> myStringArray = new ArrayList<String>();
-        myStringArray.add("First Suggestion");
-        myStringArray.add("2nd Suggestion");
-        myStringArray.add("3rd Suggestion");
-        myStringArray.add("4th Suggestion");
-        myStringArray.add("5th Suggestion");
-        myStringArray.add("6th Suggestion");
-        myStringArray.add("7th Suggestion");
-        myStringArray.add("8th Suggestion");
-        myStringArray.add("9th Suggestion");
-        myStringArray.add("10th Suggestion");
-        myStringArray.add("11th Suggestion");
-        myStringArray.add("12th Suggestion");
-       
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray);
-        ListView listView = (ListView) findViewById(R.id.listView1);
-        listView.setAdapter(adapter);  
-	}
-	
-	private void displayNewMessage() {
-
-		// get the message
+		// get the new message if any
 		bundle = getIntent().getExtras();
-		newMessage = "no message";
 		if (bundle != null) {
 			newMessage = bundle.getString("key");
+			displayNewMessage(newMessage);
+			messageArray.add(newMessage);
 		}
+
+		// display all messages
+		Collections.reverse(messageArray);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, messageArray);
+		ListView listView = (ListView) findViewById(R.id.listView1);
+		listView.setAdapter(adapter);
+
+	}
+	
+	private void displayNewMessage(String message) {
 
 		// add a dialog and set message
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
@@ -60,15 +65,14 @@ public class MessageCenterActivity extends Activity {
 					public void onClick(DialogInterface dialog, int which) {
 						// if "OK" is clicked, close the dialog window
 						dialog.cancel();
+
 					}
 				});
 
 		// create and show the alert dialog
 		AlertDialog alertDialog = alertDialogBuilder.create();
 		alertDialog.show();
-		
 
-		// TODO: after the message window is dismissed, add the new message to list
 	}
 
 }
