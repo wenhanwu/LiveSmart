b0679a580ef9956576787d23fc8fb06b37db7456
Jenny Wang <mujenwang971@gmail.com>
2014-03-15 20:44:54 -0700

    Revert "trying to remove the .classpath file"
    This reverts commit 89b54709f8ec92ae1e406ab4bad7241fa754e799, reversing
    changes made to 488866931f606522b48f87e30eacfe65fd9bcb84.

--- src/com/mss/livesmart/HealthCenterActivity.java
+++ src/com/mss/livesmart/HealthCenterActivity.java
@@ -3,7 +3,7 @@ package com.mss.livesmart;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,7 +18,10 @@ public class HealthCenterActivity extends Activity {
         getRecommendaitons.setOnClickListener(new View.OnClickListener() {
  
             public void onClick(View arg0) {
-           	
+ 
+//            	DummyEngine dummyEngine = new DummyEngine();
+//            	String recommendation = dummyEngine.generateRecommendation();
+            	
             	JsonTest dummyJson = new JsonTest();
             	Response[] responseList = dummyJson.jsonToList();
             	
