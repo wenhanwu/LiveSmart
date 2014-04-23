75058e0adfac0fcebfb77415ffbb808648a3fe96
Wenhan Wu <wenhanwzh@gmail.com>
2014-04-23 09:44:54 -0700

    Finalized version
    Finalized version

--- src/com/mss/livesmart/utils/RecomProcessor.java
+++ src/com/mss/livesmart/utils/RecomProcessor.java
@@ -1,10 +1,15 @@
 package com.mss.livesmart.utils;
 
+import com.mss.livesmart.sampledata.CurUserInfo;
+
+//The data are currently in code. For the future improvement, we will put the data in configuration file for a better extensibility.
 public class RecomProcessor {
 	public static String process(String rawRecom) {
+		// More data for recommendation will be considered in future development
 		if (rawRecom
-				.contains("blood pressure kept higher than 140 in the last month")) {
-			rawRecom += "\nBetter to take some pills.";
+				.contains("blood pressure kept higher than 140 in the last month")
+				&& CurUserInfo.getUserInfo().isHypertension()) {
+			rawRecom += "\nBetter to take some pills or see your doctor.";
 		}
 		return rawRecom;
 	}
